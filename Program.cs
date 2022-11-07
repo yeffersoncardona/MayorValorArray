816d34fd3d959b89ac4b24e5d9033689eeafdfb9
yefferson Cardona <yeffercardona@hotmail.com>
2022-11-07 07:26:14 -0500

    valor mas alto array

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DigitalWarePrueba
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5];
+            int  max = myArray[0];
+            Random rnd = new Random();
+            int valor = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                valor = rnd.Next(1, 101);
+
+                myArray[i] = valor ;
+
+                if (valor > max)
+                {
+                    max = valor;
+                }
+            }
+
+            Console.Write(max);
+           
+            Console.ReadKey();
+        }
+
+
+    }
+}
